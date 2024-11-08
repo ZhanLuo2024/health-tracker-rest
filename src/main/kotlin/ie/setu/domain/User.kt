3c8e31da99adf82dd36ce666715467e6c446d3e1
ZhanLuo2024 <20109459@mail.wit.ie>
2024-11-08 21:47:18 +0000

    branch Userlogin first commit

--- src/main/kotlin/ie/setu/domain/User.kt
+++ src/main/kotlin/ie/setu/domain/User.kt
@@ -1,3 +1,10 @@
 package ie.setu.domain
 
 data class User(var id: Int, var name: String, var email: String)
+
+// This class is used specifically for user registration.
+data class UserRegistration(
+    val name: String,
+    val email: String,
+    val password: String
+)
