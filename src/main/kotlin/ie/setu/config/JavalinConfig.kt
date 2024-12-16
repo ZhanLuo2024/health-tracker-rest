b48f4e1ee3c3fda47e38dbaf6733726e5b661636
ZhanLuo <20109459@mail.wit.ie>
2024-12-16 13:35:17 +0000

    add vue endpoint and views

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -8,12 +8,15 @@ import ie.setu.routes.registerUserRegisterRoute
 import io.javalin.Javalin
 import io.javalin.json.JavalinJackson
 import ie.setu.utils.jsonObjectMapper
+import io.javalin.vue.VueComponent
 
 class JavalinConfig {
     fun startJavalinService(): Javalin {
         val app = Javalin.create {
             //add this jsonMapper to serialise objects to json
             it.jsonMapper(JavalinJackson(jsonObjectMapper()))
+            it.staticFiles.enableWebjars()
+            it.vue.vueInstanceNameInJs = "app" // only required for Vue 3, is defined in layout.html
         }
             .apply{
                 exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
@@ -22,6 +25,7 @@ class JavalinConfig {
             .start(getRemoteAssignedPort())
 
         registerRoutes(app)
+        registerHomePage(app)
         return app
     }
 
@@ -41,6 +45,18 @@ class JavalinConfig {
         // register user register routes
         registerUserRegisterRoute(app)
 
+    }
+
+    private fun registerHomePage(app: Javalin) {
+        // The @routeComponent that we added in layout.html earlier will be replaced
+        // by the String inside the VueComponent. This means a call to / will load
+        // the layout and display our <home-page> component.
+        app.get("/", VueComponent("<home-page></home-page>"))
+        app.get("/users", VueComponent("<user-overview></user-overview>"))
+        app.get("/users/{user-id}", VueComponent("<user-profile></user-profile>"))
+
+        //activity
+        app.get("/users/{user-id}/activities", VueComponent("<user-activity-overview></user-activity-overview>"))
     }
 
     private fun getRemoteAssignedPort(): Int {
