d1ad80dd76084c0db6b2df3fbcc0513451bba8cc
ZhanLuo <20109459@mail.wit.ie>
2024-12-23 21:23:28 +0000

    bug fix

---
+++ src/main/resources/vue/views/user-login.vue
@@ -0,0 +1,25 @@
+
+<!--suppress JSAnnotator -->
+<template id="user-login">
+
+</template>
+
+<script>
+app.component("user-login", {
+  template: "#user-login",
+  data: () => ({
+
+  }),
+  created() {
+    this.fetchWaterIntakeRecords();
+  },
+  methods: {
+
+  },
+});
+</script>
+
+
+<style scoped>
+
+</style>
